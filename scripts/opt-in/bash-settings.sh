8ed9115da2b4b4becfa5f595185ac139a29dca05
James Hart <james.hart@voom.flights>
2019-09-17 17:23:02 -0700

    Properly install nvm, node, and right version of ruby

--- scripts/opt-in/bash-settings.sh
+++ scripts/opt-in/bash-settings.sh
@@ -1,3 +1,9 @@
 cat ~/.bash_profile | grep 'rbenv init' || echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
+cat ~/.bash_profile | grep 'export NVM_DIR' || echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
+cat ~/.bash_profile | grep '$NVM_DIR/nvm.sh' || echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bash_profile
+cat ~/.bash_profile | grep '$NVM_DIR/bash_completion' || echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.bash_profile
+  # This loads nvm
+
+
 cp files/custom.aliases.bash ~/.bash_it/aliases/enabled/custom.aliases.bash
--- scripts/opt-in/ruby.sh
+++ scripts/opt-in/ruby.sh
@@ -1,10 +1,10 @@
 echo
-echo "Installing Ruby tools and Ruby 2.5.1"
+echo "Installing Ruby tools and Ruby 2.6.4"
 cp files/.irbrc ~/.irbrc
 brew_install_if_missing readline
 eval "$(rbenv init -)"
-rbenv install 2.5.1 --skip-existing
-rbenv global 2.5.1
-gem install bundler:'< 2'
+rbenv install 2.6.4 --skip-existing
+rbenv global 2.6.4
+gem install bundler --version 2.0.2
 rbenv rehash
 
